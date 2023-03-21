2a656f8e320a534dc7f569e381de0b367d8b2013
Kishan Shiyal <kishan.shiyal@hotmail.co.uk>
2023-03-21 14:07:22 +0000

    Removed the public modifiers as JUnit 5 does not need it

--- src/test/java/com/softwareinstitute/kishan/shiyal/animal/tests/CatTest.java
+++ src/test/java/com/softwareinstitute/kishan/shiyal/animal/tests/CatTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class CatTest {
+class CatTest {
 
     @Test
     void testing_cat_age(){
